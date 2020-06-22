b6b8fc602268b7e19f8b129abe56fa1dbf5ce77c
LuciEN <lucien.liu@users.noreply.github.com>
2020-06-21 22:22:55 -0400

    updated comments

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ UPLOAD_FOLDER = '///musicxmlCache'
 ALLOWED_EXTENSIONS = {'musicxml'}
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDataBase.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Convert(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.TEXT, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -22,46 +22,54 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+#only accepting limited file formats
+#format whitelist is in ALLOWED_EXTENSIONS
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
         converted_text = ''
-        f = io.open("temp.musicxml", "w+", encoding='utf-8')
-        f.write(task_content)
-        f.close()
+        
+        #get user input and write into a file
+        task_content = request.form['content']
+        temp_inputfile = io.open("temp.musicxml", "w+", encoding='utf-8')
+        temp_inputfile.write(task_content)
+        temp_inputfile.close()
+        
+        #execute the converter script and listen for result
         process = Popen(['python', 'xml2abc.py', 'temp.musicxml'], stdout=PIPE, stderr = PIPE, encoding='utf-8')
         stdout, stderr = process.communicate()
         result = stdout
-        if(result==''):
-            result = stderr + '\n' + "There is something wrong with your input. Please check again!\n"
-        else:  
-            f2 = io.open("result.abc", "r+", encoding='utf-8')
-            converted_text = f2.read()
-            f2.close()
         
-        new_task = Todo(content= result + converted_text) 
+
+        if(process.returncode!=0): #display error message
+            result = stderr + '\n' + "There is something wrong with your input. Please check again!\n"
+        else: #write result text into a file for future access
+            temp_outputfile = io.open("result.abc", "r+", encoding='utf-8')
+            converted_text = temp_outputfile.read()
+            temp_outputfile.close()
+        
+        #put result in a new Convert instance
+        new_task = Convert(content= result + converted_text) 
 
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your fact!!'
+            return 'There was an issue appending the result!!'
         os.remove('temp.musicxml')
         os.remove('result.abc')
 
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = Convert.query.order_by(Convert.date_created).all()
         return render_template('index.html', tasks=tasks)
 
-def upload_file():
+def upload_file(): #still in development
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -81,7 +89,7 @@ def upload_file():
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Convert.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -92,7 +100,7 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task = Convert.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
