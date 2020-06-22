48b6bec1b57680ce3c40a7dfb5cbed8bc5f35894
LuciEN <lucien.liu@users.noreply.github.com>
2020-06-21 21:26:50 -0400

    now support special characters

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from subprocess import Popen, PIPE
-import os
+import os, io
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '///musicxmlCache'
+ALLOWED_EXTENSIONS = {'musicxml'}
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -15,21 +22,24 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        f = open("temp.musicxml", "a")
+        f = io.open("temp.musicxml", "w+", encoding='utf-8')
         f.write(task_content)
         f.close()
-        process = Popen(['python', 'xml2abc.py', 'temp.musicxml'], stdout=PIPE, stderr = PIPE)
+        process = Popen(['python', 'xml2abc.py', 'temp.musicxml'], stdout=PIPE, stderr = PIPE, encoding='utf-8')
         stdout, stderr = process.communicate()
-        result = stdout.decode('UTF-8')
+        result = stdout
         if(result==''):
-            result = "There is something wrong with your input. Please check again!"
-        new_task = Todo(content=result) 
-        os.remove("temp.musicxml")
+            result = stderr + '\n' + "There is something wrong with your input. Please check again!"
+        new_task = Todo(content= result) 
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -41,6 +51,23 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
 
 @app.route('/delete/<int:id>')
 def delete(id):
