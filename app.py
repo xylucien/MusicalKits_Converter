c1f6f3dc36673195d564d2a74a06fbea4d2d2cd4
LuciEN <lucien.liu@users.noreply.github.com>
2020-06-23 19:58:47 -0400

    basic upload function implremented / revise database structure and front end logic

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class Convert(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.TEXT, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
+    is_file = db.Column(db.Integer, default=0)
     def __repr__(self):
         return '<Task %r>' % self.id
 
@@ -35,28 +35,7 @@ def allowed_file(filename):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        converted_text = ''
-        
-        #get user input and write into a file
-        task_content = request.form['content']
-        with io.open("temp.musicxml", "w+", encoding='utf-8') as temp_inputfile:
-            temp_inputfile.write(task_content)
-        
-        #execute the converter script and listen for result
-        process = Popen(['python', 'xml2abc.py', 'temp.musicxml'], stdout=PIPE, stderr = PIPE, encoding='utf-8')
-        stdout, stderr = process.communicate()
-        result = stdout
-        
-        #display error message
-        if(process.returncode!=0): 
-            result = stderr + '\n' + "There is something wrong with your input. Please check again!\n"
-        #write result text into a file for future access
-        else: 
-            with io.open(output_file, "r+", encoding='utf-8') as temp_outputfile:
-                converted_text = temp_outputfile.read()
-        
-        #put result in a new Convert instance
-        new_task = Convert(content= result + converted_text) 
+        new_task = Convert(content = task_content)
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -67,7 +46,7 @@ def index():
             os.remove(input_file)
             os.remove(output_file)
         except OSError as error: 
-            print(error + 'n' + "File path can not be removed") 
+            flash(error + 'n' + "File path can not be removed") 
 
     else:
         tasks = Convert.query.order_by(Convert.date_created).all()
@@ -91,6 +70,10 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new_task = Convert(content = filename, is_file = 1)
+            db.session.add(new_task)
+            db.session.commit()
+
             # prompt that upload is successful
             # may add more features in this page later
             return render_template("upload.html")
@@ -110,18 +93,41 @@ def delete(id):
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Convert.query.get_or_404(id)
+    converted_text = ''
+
+    #if user copy-pasted
+    if task.is_file == 0:
+        #get user input and write into a file
+        task_content = task.content
+        with io.open("temp.musicxml", "w+", encoding='utf-8') as temp_inputfile:
+            temp_inputfile.write(task_content)
+        
+        #execute the converter script and listen for result
+        process = Popen(['python', 'xml2abc.py', 'temp.musicxml'], stdout=PIPE, stderr = PIPE, encoding='utf-8')
+    
+    #if user uploaded a file
+    else:
+        process = Popen(['python', 'xml2abc.py', os.path.join(UPLOAD_FOLDER,task.content)], stdout=PIPE, stderr = PIPE, encoding='utf-8')
+
+    stdout, stderr = process.communicate()
+    result = stdout
+    
+    #display error message
+    if(process.returncode!=0): 
+        result = stderr + '\n' + "There is something wrong with your input. Please check again!\n"
+    #write result text into a file for future access
+    else: 
+        with io.open(output_file, "r+", encoding='utf-8') as temp_outputfile:
+            converted_text = temp_outputfile.read()
+    
+    #put result in a new Convert instance
+    new_task = Convert(content= result + converted_text) 
 
     if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your fact!'
+        return redirect('/')
 
     else:
-        return render_template('update.html', task=task)
+        return render_template('update.html', task=new_task)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
