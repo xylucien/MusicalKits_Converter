ec6ae5e0c42e862fbf6e6d5a45248255baca904f
LuciEN <lucien.liu@users.noreply.github.com>
2020-06-23 18:13:15 -0400

    Added bootstrap for the sake of beauty (and to impletement flash messages)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from subprocess import Popen, PIPE
@@ -9,6 +9,8 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(),'musicxmlCache')
 ALLOWED_EXTENSIONS = {'musicxml'}
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDataBase.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -73,23 +75,26 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file(): 
-    #still in development
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return redirect('/')
         file = request.files['file']
+
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
+            return redirect('/')
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # prompt that upload is successful
+            # may add more features in this page later
             return render_template("upload.html")
-    return render_template('index.html')
+    return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
