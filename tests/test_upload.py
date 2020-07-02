9a8f3aec89634dd3381dde95c91c11c88dad1a4f
LuciEN <lucien.liu@users.noreply.github.com>
2020-07-02 17:50:52 -0400

    added pytest

---
+++ tests/test_upload.py
@@ -0,0 +1,24 @@
+from converter.convert import Convert
+import io
+
+def test_upload_IncorrectMusicXml(test_client):
+    #Test can upload incorrect MusicXML
+    data = {}
+    data = {key: str(value) for key, value in data.items()}
+    data['file'] = (io.BytesIO(b"abcdef"), 'test.musicxml')
+    response = test_client.post(
+        '/convert_result/upload', data=data, follow_redirects=True,
+        content_type='multipart/form-data'
+    )
+    assert b"INVALID" in response.data
+
+def test_upload_CorrectMusicXml(test_client):
+    #Test can upload and convert valid musicxml
+    data = {}
+    data = {key: str(value) for key, value in data.items()}
+    data['file'] = open("tests/sample.musicxml", "rb")
+    response = test_client.post(
+        '/convert_result/upload', data=data, follow_redirects=True,
+        content_type='multipart/form-data'
+    )
+    assert b"Z:Copyright" in response.data
