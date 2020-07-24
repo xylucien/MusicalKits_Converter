0ec2ad3c8343d78fea4ab5332883165fe3e69617
Lucien <liuxinyubj@qq.com>
2020-07-24 02:32:10 -0400

    deleted meaninless lines

--- tests/test_image.py
+++ tests/test_image.py
@@ -3,4 +3,3 @@ from music21 import *
 def test_png():
     a = converter.parse('tests/sample.musicxml').write('lilypond.png')
     b = converter.parse('tests/sample.musicxml').write('musicxml.png')
-    assert 1 == 1
--- tests/test_sound.py
+++ tests/test_sound.py
@@ -2,4 +2,3 @@ from music21 import *
 
 def test_midi():
     a = converter.parse('result.abc').write('midi')
-    assert 1 == 1
