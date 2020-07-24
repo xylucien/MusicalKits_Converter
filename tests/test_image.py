36802f9790e060c1344d792b91091fcb3827602f
Lucien <liuxinyubj@qq.com>
2020-07-24 18:17:55 -0400

    2nd test

--- tests/test_image.py
+++ tests/test_image.py
@@ -3,5 +3,5 @@ from music21 import *
 def test_png_lilypond():
     a = converter.parse('tests/sample.musicxml').write('lilypond.png')
 def test_png_mscore():   
-    #b = converter.parse('tests/ActorPreludeSample.musicxml').write('musicxml.png')
+    b = converter.parse('tests/ActorPreludeSample.musicxml').write('musicxml.png')
     bb = converter.parse('tests/sample.musicxml').write('musicxml.png')
