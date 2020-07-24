4230d6c54242360e4340c85b48a7fe796f59f8fe
Lucien <liuxinyubj@qq.com>
2020-07-24 02:40:37 -0400

    split test

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,5 +1,6 @@
 from music21 import *
 
-def test_png():
+def test_png_lilypond():
     a = converter.parse('tests/sample.musicxml').write('lilypond.png')
+def test_png_mscore():    
     b = converter.parse('tests/sample.musicxml').write('musicxml.png')
